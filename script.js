4b3e1fcb7d9cafd3b50c489bcadfd1da866bb474
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-05-20 21:48:23 -0700

    fixing java still

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const startButton = document.getElementById('startbttn');
 const questioncontainer = document.getElementById('questcon');
 const questionelem = document.getElementById('questions')
-const answerelem = document.getElementById('answrbttns')
+const answerelem = document.getElementById('answers')
+const highscore = document.getElementById('scoreres');
+const enterel = document.getElementById('enter')
+const responseel = document.getElementById('response')
 
-let shuffquest, questionsleft
+const timer = document.getElementById('timerset');
+let timerseconds = 75;
+let userHighscore;
 
-startButton.addEventListener('click' , startquiz)
-//first lets try to start the game
-function startquiz() {
-    console.log('started')
-    startButton.classList.add('hide')
-    shuffquest = questions.sort(() => Math.random() - .5)
-    questionsleft = 0
-    questioncontainer.classList.remove('hide')
-    nextquestion()
-}
-
+const questions = [
+    {
+        question: 'What is html?',
+            answers: ["a language", "a meal", "a movie", "a place"],
+            answerCheck: 'a lanuage',
+    },
+    {
+        question: 'Why use it?',
+            answers: ['to make a website', 'who knows'],
+            answerCheck:'to make a website',
+    },
+    {
+        question: 'When was the computer first invented?',
+            answers: ['1990', '1921', '1936','1800'],
+            answerCheck: '1990',
+    },
+    {
+        question: 'Why code?',
+            answers: ['because its fun', 'who knows'],
+            answerCheck:'because its fun',
+    },
+    {
+        question: 'How many hours should you sleep?',
+            answers: ['1990', '1921', '1936','1800'],
+            answerCheck: '1990',
+    },
+]
 function nextquestion() {
     showquestion(shuffquest[questionsleft])
 }
@@ -45,15 +66,65 @@ function chooseanswr(){
 
 }
 
+let questionsleft = 0;
 
+function startquiz() {
+    document.getElementById("rules").classList.add("hide");
+    document.getElementById("questcon").classList.remove("hide");
+    //questionsleft = 0
+    // timerStart();
+    // askquestions();
+}
 
-//makequestions
-const questions = [
-    {
-        question: 'What is a local variable?',
-        answers: [
-            {text:'this',correct: true },
-            {text: 'this', correct: false},
-        ]
+function timerStart() {
+    timer = setInterval(() => {
+    timerseconds--;
+        timer.textContent = "Time: " + timerseconds + " seconds remaining!";
+        if (timerseconds === 0 || questionsleft == questions.length) {
+          clearInterval(timer);
+          setTimeout(displayScore);
+        }
+      }, 1000);
+};
+timerStart()
+
+function displayScore () {
+    document.getElementById("questcon").classList.add("hidden");
+    document.getElementById("response").classList.remove("hidden");
+    document.getElementById("scoreres").classList.remove("hidden");
+    document.getElementById("sub").classList.remove("hidden");
+  
+    let userscore = document.createElement("input");
+    let scoresub = document.createElement("button");
+    highscore.appendChild(userscore);
+    enterel.appendChild(scoresub);
+    highscoresub.textContent = "enter"
+    highscoresub.classList.add("score-submit")
+
+    enterel.textContent = "Your Score: " + timeLeft;
+    let userHighscore = JSON.stringify(userscore.textContent);
+};
+displayScore();
+
+answerelem.addEventListener('click', (event) => {
+    if (answer === event.target.textContent) {
+        responsel.textContent = "Right on!";
+        setTimeout(hideResponse,1000);
     }
-]
+    else {
+        responseel.textContent = "Oops not correct!";
+        timerseconds = timerseconds - 10;
+    }
+    showResponse();
+    askquestions();
+    }
+);
+
+responseel.addEventListener('click', () => {
+    if (typeof Storage !== 'undefined'){
+        localStorage.setItem('score',timerseconds);
+        localStorage.setItem('user',userHighscore);
+    }
+});
+
+startButton.addEventListener('click', startquiz)
